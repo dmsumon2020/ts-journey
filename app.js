13791147b14b91a6a73b9e4f441f65af8dfcad78
dmsumon2020 <dmsumon2020@gmail.com>
2025-06-25 20:15:47 +0600

    added app.ts

---
+++ app.js
@@ -0,0 +1,5 @@
+var userName = "Sumon";
+var userAge = 25;
+var userCountry = "Bangladesh";
+var userCity = "Dhaka";
+console.log(userName, userAge, userCountry, userCity);
---
+++ app.ts
@@ -0,0 +1,6 @@
+var userName: string = "Sumon";
+var userAge: number = 25;
+var userCountry: string = "Bangladesh";
+var userCity: string = "Dhaka";
+
+console.log(userName, userAge, userCountry, userCity);
